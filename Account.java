b928bc096080563c71f08d8a58bdb8b119a0e242
Ravindra Singh Shekhawat <shekhawatravi@gmail.com>
2021-10-18 21:11:56 +0530

    combining last commit with --amend command.

--- Account.java
+++ Account.java
@@ -1,2 +1,3 @@
 First Account file.
 second line in Account file.
+Adding third line, to test git commit --amend command with 2 different commits.
--- Saving.java
+++ Saving.java
@@ -1,2 +1,3 @@
 This is saving java file.
 second line in saving file.
+In Saving.java - Adding third line, to test git commit --amend command with 2 different commits.
